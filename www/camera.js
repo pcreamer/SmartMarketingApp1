5b6869b31a379774f977e757a8ed441b2e99a310
unknown <Paul@Paul-PC.(none)>
2012-01-30 19:47:31 +0000

    Initial

---
+++ www/camera.js
@@ -0,0 +1,18 @@
+﻿       function drawdiagonal()
+       {
+       console.log("Hi");
+       var canvas=document.getElementById('pc1');
+       var context=canvas.getContext('2d');
+       context.beginPath();
+       context.rect(0, 0, 100, 100);
+       context.moveTo(70,140);
+       context.lineTo(140,70);
+       context.lineWidth=10;
+       context.strokeStyle = 'blue';
+       context.fillStyle = 'yellow';
+       context.fill();
+       context.stroke();
+
+       window.addEventListener("load",drawdiagonal,false);
+
+       };
